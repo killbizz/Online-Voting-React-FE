26b7380b70ec80dfc7441d47559cc69728fe042e
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-21 18:18:19 +0200

    add homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,29 @@
-import Link from 'next/link'
 import Layout from '../components/Layout'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const IndexPage = () => (
   <Layout title="e-Voting Homepage">
     <div className="mid">
-      <h1 className="text-center">Hello World 👋</h1>
-      <p className="text-center">
-        <Link href="/about">
-          <a>DAGHE</a>
-        </Link>
-      </p>
+      <div className="container col-xxl-8 px-4 py-5">
+        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div className="col-10 col-sm-8 col-lg-5">
+            <Image src="/images/colored-ballots_bw.jpg" className="d-block mx-lg-auto img-fluid" alt="Voting Image" width="1200" height="900" loading="lazy" />
+          </div>
+          <div className="col-lg-6">
+            <h1 className="display-5 fw-bold lh-1 mb-3">Welcome to the Online Voting Web Application</h1>
+            <p className="lead">In this website you can <b>Sign Up</b> to become a registered user and take part in all <b>elections</b> open to date. The admin of the platform can use the appropriate <b>dashboard</b> to manage the votes and the political parties.</p>
+            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+              <Link href="/sign-up">
+                <a><button type="button" className="btn btn-danger btn-lg px-4 me-md-2">Sign Up to Vote!</button></a>
+              </Link>
+              <Link href="/login">
+                <a type="button" className="btn btn-outline-secondary btn-lg px-4">Login with your credentials</a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </Layout>
 )
