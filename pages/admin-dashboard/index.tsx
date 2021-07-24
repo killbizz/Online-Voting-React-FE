72cd5b4155a5fb191515067fb7823124ca7fcd7f
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-24 21:14:20 +0200

    add server side data fetch and authorization using cookies

--- pages/admin-dashboard/index.tsx
+++ pages/admin-dashboard/index.tsx
@@ -1,6 +1,15 @@
+import { GetServerSideProps, GetStaticPropsResult, NextApiRequest } from 'next';
+import NewElection from '../../components/admin/NewElection';
 import Layout from '../../components/Layout'
+import { AuthService } from '../../services/auth.service';
+import { PartyService } from '../../services/party.service';
+import { Party } from '../../classes/Party';
 
-const AdminDashboard = () => {
+interface AdminDashboardProps {
+  parties: Party[]
+}
+
+const AdminDashboard = ({ parties } : AdminDashboardProps) => {
     return (
         <Layout title="Admin Dashboard">
             <div className="mid">
@@ -8,13 +17,31 @@ const AdminDashboard = () => {
                     <div>
                         <h1 className="text-center mb-4">Admin Dashboard</h1>
                         <h3 className="text-center my-4">Elections</h3>
-                        
+                        <NewElection />
                         <h3 className="text-center my-4">Political Parties</h3>
                     </div>
                 </div>
             </div>
         </Layout>
     );
-}
+};
+
+export const getServerSideProps: GetServerSideProps<AdminDashboardProps> = async ({ req }): Promise<GetStaticPropsResult<AdminDashboardProps>> => {
+  const partyService: PartyService = PartyService.getInstance();
+  // session handling using cookies
+  if(!(req.cookies.jwtToken !== undefined && req.cookies.userRole === "admin")) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      parties: await partyService.getParties()
+    }
+  };
+};
   
-  export default AdminDashboard
+  export default AdminDashboard;
