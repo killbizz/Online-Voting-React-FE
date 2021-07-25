79ecee35f5dbb42f68693cd8032309d9498276c4
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-25 16:29:10 +0200

    add PartyList

--- components/admin/PartyList.tsx
+++ components/admin/PartyList.tsx
@@ -1,5 +1,58 @@
-const PartyList = () => {
+import { Party } from "../../classes/Party"
+import ReactTooltip from 'react-tooltip';
 
+interface PartyListProps {
+    parties: Party[]
+}
+
+const PartyList = ({ parties }: PartyListProps) => {
+
+    const deletePossibility = (id: number) => {
+        return false;
+    }
+
+    const deleteParty = (id: number) => {
+
+    }
+
+    return(
+        <table className="table mb-4">
+            <thead className="thead-light">
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Logo</th>
+                <th scope="col">Name</th>
+                <th scope="col">Candidate</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                { parties.map((party) => 
+                    <tr>
+                        <th scope="row">{party.id}</th>
+                        <td><img className="rounded" src={party.base64logo} alt={party.name} height="100" width="60" /></td>
+                        <td>{party.name.charAt(0).toUpperCase() + party.name.slice(1)}</td>
+                        <td>{party.candidate.charAt(0).toUpperCase() + party.candidate.slice(1)}</td>
+                        <td>
+                            { !deletePossibility(party.id) &&
+                            <>
+                                <ReactTooltip place="right" />
+                                <span className="d-inline-block" tabIndex={0} data-tip="This party is present in an old votation or in one that is currently open">
+                                    <button type="button" disabled={deletePossibility(party.id) ? undefined : true} className="btn btn-danger btn-sm px-4 me-md-2" onClick={ () => deleteParty(party.id)}>Delete</button>
+                                </span>
+                            </>
+                            }
+                            { deletePossibility(party.id) &&
+                            <button type="button" disabled={deletePossibility(party.id) ? undefined : true } className="btn btn-danger btn-sm px-4 me-md-2" onClick={() => deleteParty(party.id)}>Delete</button>
+                            }
+                        </td>
+                    </tr>
+                )
+                }
+                
+            </tbody>
+        </table>
+    );
 }
 
 export default PartyList;
--- pages/admin-dashboard/index.tsx
+++ pages/admin-dashboard/index.tsx
@@ -6,6 +6,7 @@ import { Party } from '../../classes/Party';
 import ElectionList from '../../components/admin/ElectionList';
 import { getElections } from '../../services/election';
 import { Election } from '../../classes/Election';
+import PartyList from '../../components/admin/PartyList';
 
 interface AdminDashboardProps {
   parties: Party[],
@@ -16,13 +17,14 @@ const AdminDashboard = ({ parties, elections } : AdminDashboardProps) => {
     return (
         <Layout title="Admin Dashboard">
             <div className="mid">
-                <div className="container col-xxl-8 py-3">
+                <div className="containerDashboard col-xxl-8 py-3">
                     <div>
                         <h1 className="text-center mb-4">Admin Dashboard</h1>
                         <h3 className="text-center my-4">Elections</h3>
                         <NewElection parties={parties} />
                         <ElectionList elections={elections} />
                         <h3 className="text-center my-4">Political Parties</h3>
+                        <PartyList parties={parties} />
                     </div>
                 </div>
             </div>
