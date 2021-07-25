6ec224b64349d133d313d490830d72f1fabf3c20
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-25 19:31:40 +0200

    add NewParty component

---
+++ components/admin/NewParty.tsx
@@ -0,0 +1,178 @@
+import { useState } from "react";
+import {
+    Button,
+    Accordion,
+    Card,
+    Form,
+    Row,
+    Col,
+    FormControl,
+    FormGroup,
+    FormLabel,
+    Alert
+  } from "react-bootstrap";
+import { Party } from "../../classes/Party";
+import { fileToBase64, newParty } from "../../services/party.service";
+
+interface NewPartyProps {
+    refreshOnPartiesChange: any
+}
+
+const NewParty = ({ refreshOnPartiesChange }: NewPartyProps) => {
+
+    const createNewParty = async (event: any) => {
+        event.preventDefault();
+
+        const name: string = event.target.name.value;
+        const candidate: string = event.target.candidate.value;
+        const logoFile: string | undefined = event.target.logo.files[0];
+
+        const valid: boolean = formValidation(name, candidate, logoFile);
+
+        if(valid){
+            const base64logo = await fileToBase64(logoFile);
+
+            const party: Party = new Party(0, name, candidate, base64logo);
+            const result: boolean = await newParty(party);
+            refreshOnPartiesChange();
+            setPartyCreated(result);
+        }
+    }
+
+    const formValidation = (name: string, candidate: string, logo: string | undefined): boolean => {
+        let isValid: boolean = true;
+        if (name === "") {
+          updateErrors("nameError", "The Name field is required");
+          isValid = false;
+        }
+        else if (name.length < 6) {
+          updateErrors("nameError", "The Name field must contain at least 6 characters");
+          isValid = false;
+        }
+        else {
+          updateErrors("nameError", "");
+        }
+        if (candidate === "") {
+            updateErrors("candidateError", "The Candidate field is required");
+            isValid = false;
+        }
+        else if (name.length < 6) {
+        updateErrors("candidateError", "The Candidate field must contain at least 6 characters");
+        isValid = false;
+        }
+        else {
+        updateErrors("candidateError", "");
+        }
+        if (logo === undefined) {
+            updateErrors("logoError", "The Party Logo is required");
+            isValid = false;
+        }
+        else {
+        updateErrors("logoError", "");
+        }
+        return isValid;
+      }
+    
+      // passing a clone o errors map to setErrors in order to trigger the state update
+      const updateErrors = (key: string, value: string) => {
+        setErrors(new Map<string,string>(errors.set(key,value)));
+      }
+
+    const [errors, setErrors] = useState(new Map<string,string>());
+    const [show, setShow] = useState(true);
+    const [partyCreated, setPartyCreated] = useState(false);
+
+    return (
+        <>
+      <Accordion>
+        <Card>
+          <Card.Header style={{ backgroundColor: "white" }}>
+            <div className="d-flex justify-content-center">
+              <Accordion.Toggle as={Button} variant="secondary" eventKey="1">
+                Add new party
+              </Accordion.Toggle>
+            </div>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>
+              <Form onSubmit={createNewParty}>
+                <FormGroup as={Row}>
+                  <FormLabel column sm="2" htmlFor="name">
+                    Party Name
+                  </FormLabel>
+                  <Col sm="10">
+                    <FormControl
+                      type="text"
+                      className="sm text-center"
+                      id="name"
+                      name="name"
+                      placeholder="Name"
+                    />
+                    {errors.has("nameError") &&
+                      <small id="nameError" className="text-danger">
+                        {errors.get("nameError")}
+                      </small>
+                    }
+                  </Col>
+                </FormGroup>
+                <FormGroup as={Row}>
+                  <FormLabel column sm="2" htmlFor="type">
+                    Candidate
+                  </FormLabel>
+                  <Col sm="10">
+                    <FormControl
+                        type="text"
+                        className="sm text-center"
+                        id="candidate"
+                        name="candidate"
+                        placeholder="Candidate"
+                        />
+                    {errors.has("candidateError") &&
+                      <small id="candidateErrors" className="text-danger">
+                        {errors.get("candidateError")}
+                      </small>
+                    }
+                  </Col>
+                </FormGroup>
+                <FormGroup as={Row}>
+                  <FormLabel column className="mt-4" sm="2" htmlFor="type">
+                    Party Logo
+                  </FormLabel>
+                  <Col sm="10">
+                    <FormControl
+                        type="file"
+                        className="sm text-center mt-4"
+                        id="logo"
+                        name="logo"
+                        />
+                    {errors.has("logoError") &&
+                      <small id="logoErrors" className="text-danger">
+                        {errors.get("logoError")}
+                      </small>
+                    }
+                  </Col>
+                </FormGroup>
+                <Form.Row className="text-center">
+                  <Col sm="12">
+                    <Button className="mt-4" type="submit" variant="primary" onClick={() => setShow(true)}>
+                      Submit
+                    </Button>
+                  </Col>
+                </Form.Row>
+              </Form>
+              {partyCreated &&
+                <Alert variant="success" show={show} onClose={() => setShow(false)} dismissible>
+                  <Alert.Heading className="text-center">
+                    Party created successfully!
+                  </Alert.Heading>
+                </Alert>
+              }
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
+    </>
+    );
+}
+
+export default NewParty;
--- pages/admin-dashboard/index.tsx
+++ pages/admin-dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, GetStaticPropsResult, NextApiRequest } from 'next';
+import { GetServerSideProps, GetStaticPropsResult } from 'next';
 import NewElection from '../../components/admin/NewElection';
 import Layout from '../../components/Layout'
 import { getParties } from '../../services/party.service';
@@ -7,29 +7,46 @@ import ElectionList from '../../components/admin/ElectionList';
 import { getElections } from '../../services/election';
 import { Election } from '../../classes/Election';
 import PartyList from '../../components/admin/PartyList';
+import NewParty from '../../components/admin/NewParty';
+import { useState } from 'react';
 
 interface AdminDashboardProps {
-  parties: Party[],
-  elections: Election[]
+  partiesArray: Party[],
+  electionsArray: Election[]
 }
 
-const AdminDashboard = ({ parties, elections } : AdminDashboardProps) => {
-    return (
-        <Layout title="Admin Dashboard">
-            <div className="mid">
-                <div className="containerDashboard col-xxl-8 py-3">
-                    <div>
-                        <h1 className="text-center mb-4">Admin Dashboard</h1>
-                        <h3 className="text-center my-4">Elections</h3>
-                        <NewElection parties={parties} />
-                        <ElectionList elections={elections} />
-                        <h3 className="text-center my-4">Political Parties</h3>
-                        <PartyList parties={parties} />
-                    </div>
+const AdminDashboard = ({ partiesArray, electionsArray } : AdminDashboardProps) => {
+
+  const refreshOnElectionsChange = async () => {
+    const freshElections = await getElections();
+    setElections(freshElections);
+  };
+
+  const refreshOnPartiesChange = async () => {
+    const freshParties = await getParties();
+    setParties(freshParties);
+  };
+
+  const [parties, setParties] = useState(partiesArray);
+  const [elections, setElections] = useState(electionsArray);
+
+  return (
+    <Layout title="Admin Dashboard">
+        <div className="mid">
+            <div className="containerDashboard col-xxl-8 py-3">
+                <div>
+                    <h1 className="text-center mb-4">Admin Dashboard</h1>
+                    <h3 className="text-center my-4">Elections</h3>
+                    <NewElection parties={parties} refreshOnElectionsChange={refreshOnElectionsChange} />
+                    <ElectionList elections={elections} />
+                    <h3 className="text-center my-4">Political Parties</h3>
+                    <NewParty refreshOnPartiesChange={refreshOnPartiesChange} />
+                    <PartyList parties={parties} elections={elections} refreshOnPartiesChange={refreshOnPartiesChange} />
                 </div>
             </div>
-        </Layout>
-    );
+        </div>
+    </Layout>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps<AdminDashboardProps> = async ({ req }): Promise<GetStaticPropsResult<AdminDashboardProps>> => {
@@ -44,8 +61,8 @@ export const getServerSideProps: GetServerSideProps<AdminDashboardProps> = async
   }
   return {
     props: {
-      parties: await getParties(),
-      elections: await getElections()
+      partiesArray: await getParties(),
+      electionsArray: await getElections()
     }
   };
 };
