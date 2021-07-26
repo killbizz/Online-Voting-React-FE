8259dd5bf51d5b38e4286210166baa50723f2f5f
killbizz <45452791+killbizz@users.noreply.github.com>
2021-07-26 17:49:34 +0200

    beginning of user dashoard

---
+++ components/user/ElectionList.tsx
@@ -0,0 +1,66 @@
+import { Election } from "../../classes/Election";
+import ReactTooltip from 'react-tooltip';
+import Router from 'next/router';
+import { Vote } from "../../classes/Vote";
+
+interface ElectionListProps {
+    elections: Election[],
+    userVotes: Vote[],
+    userId: string | null
+}
+
+const ElectionList = ({ elections, userVotes, userId }: ElectionListProps) => {
+
+    const userAlreadyVoted = (electionId: number): boolean => {
+        return userVotes.find((value) => value.electionId === electionId && value.userId === userId) !== undefined;
+    }
+
+    const votePossibility = (electionId: number, startDate: string, endDate: string) => {
+        const alreadyVoted: boolean = userAlreadyVoted(electionId);
+        const today: Date = new Date();
+        const timeCompatibility: boolean = new Date(startDate) <= today && new Date(endDate) >= today;
+        return timeCompatibility && !alreadyVoted;
+    }
+
+    const alreadyVoted: string = "You have already voted for this election";
+    const timingIssue: string = "This election is terminated or is not already openend";
+
+    return (
+        <table className="table mb-4">
+            <thead className="thead-light">
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Start Date</th>
+                <th scope="col">End Date</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+                {elections.map((election) => 
+                    <tr>
+                        <td>{election.name}</td>
+                        <td>{election.startDate}</td>
+                        <td>{election.endDate}</td>
+                        <td>
+                            { !votePossibility(election.id, election.startDate, election.endDate) &&
+                            <>
+                                <ReactTooltip place="right" />
+                                <span className="d-inline-block" tabIndex={0} data-tip={userAlreadyVoted(election.id) ? alreadyVoted : timingIssue}>
+                                    <button type="button" disabled className="btn btn-outline-secondary btn-sm px-4 me-md-2">Vote!</button>
+                                </span>
+                            </>
+                            }
+                            { votePossibility(election.id, election.startDate, election.endDate) &&
+                            <button type="button" className="btn btn-secondary btn-sm px-4 me-md-2" onClick={() => Router.push(`election/${election.id}`)}>Vote!</button>
+                            }
+                        </td>
+                    </tr>
+                )
+                }
+            </tbody>
+        </table>
+    );
+
+}
+
+export default ElectionList;
---
+++ pages/user-dashboard/index.tsx
@@ -0,0 +1,69 @@
+import { GetServerSideProps, GetStaticPropsResult } from 'next';
+import { Election } from '../../classes/Election';
+import { Party } from '../../classes/Party';
+import ElectionList from '../../components/user/ElectionList';
+import Layout from '../../components/Layout'
+import { getElections } from '../../services/election';
+import { getParties } from '../../services/party';
+import { useState } from 'react';
+import { Vote } from '../../classes/Vote';
+import { getVotesByUserId } from '../../services/vote';
+import { getUserId } from '../../services/auth';
+
+interface UserDashboardProps {
+    parties: Party[],
+    electionsArray: Election[],
+    votesOfTheUser: Vote[],
+    userId: string | null
+}
+
+const UserDashboard = ({parties,  electionsArray, votesOfTheUser, userId}: UserDashboardProps ) => {
+
+    const refreshOnElectionsChange = async () => {
+        const freshElections = await getElections();
+        setElections(freshElections);
+    };
+
+    const [elections, setElections] = useState(electionsArray);
+    const [userVotes, setUserVotes] = useState(votesOfTheUser);
+
+    return (
+        <Layout title="e-Voting User Dashboard">
+          <div className="mid">
+            <div className="containerDashboard col-xxl-8 py-3">
+                <div>
+                    <h1 className="text-center mb-4">User Dashboard</h1>
+                    <h3 className="text-center my-4">Elections</h3>
+                    <ElectionList elections={elections} userVotes={userVotes} userId={userId} />
+                    <h3 className="text-center my-4">Voting History</h3>
+                    
+                    
+                </div>
+            </div>
+        </div>
+        </Layout>
+      );
+}
+
+export const getServerSideProps: GetServerSideProps<UserDashboardProps> = async ({ req }): Promise<GetStaticPropsResult<UserDashboardProps>> => {
+    // session handling using cookies
+    if(!(req.cookies.jwtToken !== undefined && req.cookies.userRole !== "admin")) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+    const id: string | undefined = getUserId();
+    return {
+      props: {
+        parties: await getParties(),
+        electionsArray: await getElections(),
+        votesOfTheUser: await getVotesByUserId(id!),
+        userId: id === undefined ? null : id
+      }
+    };
+};
+
+export default UserDashboard;
