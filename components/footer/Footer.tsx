c5ed7a3f28bada4819c61ce95142c7d30ad3f0c4
killbizz <45452791+killbizz@users.noreply.github.com>
2022-04-29 13:00:37 +0200

    added the github repo link in the footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,16 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3">
             <div className="container text-center">
-                <span className="text-muted">&copy; 2021 Gabriel Bizzo.</span>
+                <div className="row justify-content-center">
+                    <div className="col-6">
+                        <span className="text-muted">&copy; 2021-2022 Gabriel Bizzo</span>
+                    </div>
+                    <div className="col-6">
+                        <a href="https://github.com/killbizz/Online-Voting-React-FE">
+                            <i className="fa fa-github fa-2x github-logo" />
+                        </a>
+                    </div>    
+                </div>
             </div>
         </footer>
     );
