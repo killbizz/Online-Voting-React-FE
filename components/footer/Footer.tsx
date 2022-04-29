e518e2288ec7421ce446a25fbf33cab301b200e1
killbizz <45452791+killbizz@users.noreply.github.com>
2022-04-29 21:07:25 +0200

    footer fix

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
     return (
-        <footer className="footer mt-auto py-3">
+        <footer className="footer mt-auto">
             <div className="container text-center">
                 <div className="row justify-content-center">
                     <div className="col-6">
-                        <span className="text-muted">&copy; 2021-2022 Gabriel Bizzo</span>
+                        <span className="text-muted copyright" >&copy; 2021-2022 Gabriel Bizzo</span>
                     </div>
                     <div className="col-6">
                         <a href="https://github.com/killbizz/Online-Voting-React-FE">
-                            <i className="fa fa-github fa-2x github-logo" />
+                            <i className="fa fa-github fa-3x github-logo copyright" />
                         </a>
                     </div>    
                 </div>
