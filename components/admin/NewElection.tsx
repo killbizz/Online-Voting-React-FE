23f0ab36d0639c51e43df38cf222355c7666237a
killbizz <45452791+killbizz@users.noreply.github.com>
2022-05-02 16:16:40 +0200

    fixed the css of newElection parties list

--- components/admin/NewElection.tsx
+++ components/admin/NewElection.tsx
@@ -195,9 +195,9 @@ const NewElection = ({ parties, refreshOnElectionsChange }: NewElectionProps) =>
                   <Col sm="9">
                       {
                         parties.map((party: Party) => 
-                          <div key={party.id} className="border border-secondary rounded my-1">
+                          <div key={party.id} className="row border border-secondary rounded my-1">
                             <input className="col-auto mx-2 checkbox" type="checkbox" id={party.id.toString()} value={party.id} onClick={() => handlePartyClick(party.id)} />
-                            <label className="col-auto mx-1" htmlFor={party.name.toString()}>{party.name} - {party.candidate}</label>
+                            <label className="col mx-1" htmlFor={party.name.toString()}>{party.name} - {party.candidate}</label>
                           </div>)
                       }
                   </Col>
