decfa900a1a002710981d3f257101a3c1b580e7b
Gabriel Bizzo <gabriel.bizzo@hotmail.it>
2022-05-26 12:28:29 +0200

    removed console log

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -34,8 +34,8 @@ const providers = [
                     password: credentials.password
                 }
                 const user = await getBackendResponse("login", "POST", JSON.stringify(jsonCredentials), undefined);
-                console.log("PROVIDERS: \n");
-                console.log(user)
+                // console.log("PROVIDERS: \n");
+                // console.log(user)
                 if (user.response.access_token) {
                     return user.response;
                 }
@@ -49,24 +49,24 @@ const providers = [
 
 const callbacks = {
     jwt: async ({ token, user }) => {
-        console.log("JWT: \n");
-        console.log(user)
+        // console.log("JWT: \n");
+        // console.log(user)
         if (user) {
             // This will only be executed at login. Each next invocation will skip this part.
             token.accessToken = user.access_token;
             token.refreshToken = user.refresh_token;
             token.userId = user.user_id;
         }
-        console.log("Token: \n");
-        console.log(token);
+        // console.log("Token: \n");
+        // console.log(token);
 
         const decoded = jwt.decode(token.accessToken);
-        console.log(decoded);
+        // console.log(decoded);
         // If accessTokenExpiry is 24 hours, we have to refresh token before 24 hours pass.
         //const shouldRefreshTime = Math.round((Date(decoded["exp"]) - 60 * 60 * 1000) - Date.now());
         const shouldRefresh = moment(decoded["exp"] * 1000).subtract(10, "minutes").isBefore(moment());
 
-        console.log("shouldRefreshTime: " + shouldRefresh);
+        console.log("shouldRefreshToken: " + shouldRefresh);
         // If the token is still valid, just return it.
         if (!shouldRefresh) {
             return Promise.resolve(token);
@@ -92,8 +92,8 @@ const callbacks = {
             session.user.name = "Gabriel";
             session.user.email = "admin@admin.com";
         }
-        console.log("SessionSession: \n");
-        console.log(session);
+        // console.log("SessionSession: \n");
+        // console.log(session);
         return Promise.resolve(session);
     },
 }
