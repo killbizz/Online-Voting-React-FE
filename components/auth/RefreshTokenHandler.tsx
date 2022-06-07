ac88452e9ac1c64b8d97ac46f75bbb7527a9d54c
killbizz <45452791+killbizz@users.noreply.github.com>
2022-06-07 21:01:43 +0200

    Update RefreshTokenHandler.tsx

--- components/auth/RefreshTokenHandler.tsx
+++ components/auth/RefreshTokenHandler.tsx
@@ -12,8 +12,6 @@ const RefreshTokenHandler = (props : RefreshTokenHandlerProps) => {
 
     useEffect(() => {
         if(!!session) {
-            // We did set the token to be ready to refresh after 23 hours, here we set interval of 23 hours 30 minutes.
-            // const timeRemaining = Math.round((((session.accessTokenExpiry - 30 * 60 * 1000) - Date.now()) / 1000));
 
             // We did set the token to be ready to refresh after 25 minutes, here we set refetch interval of 27 minutes.
             const decodedAccessToken: any = jwt.decode(session.accessToken);
@@ -24,9 +22,8 @@ const RefreshTokenHandler = (props : RefreshTokenHandlerProps) => {
                 expDate = moment();
             }
             const timeRemaining: number = Math.round(moment.duration(expDate.subtract(4, "minutes").diff(moment())).asSeconds());
-            // props.setInterval(timeRemaining > 0 ? timeRemaining : 0);
-            // console.log("TimeRemaining " + timeRemaining);
-            // console.log(expDate.toString());
+            props.setInterval(timeRemaining > 0 ? timeRemaining : 0);
+            
         }
     }, [session]);
 
