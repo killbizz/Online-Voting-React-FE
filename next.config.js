ad1c0429c61777f6ed7c4f9557a89e05a4ec3766
killbizz <45452791+killbizz@users.noreply.github.com>
2022-06-08 16:38:19 +0200

    added some security headers

---
+++ next.config.js
@@ -0,0 +1,24 @@
+// next.config.js
+
+const securityHeaders = [
+    {
+        key: 'X-XSS-Protection',
+        value: '1; mode=block'
+    },
+    {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff'
+    }   
+];
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+}
