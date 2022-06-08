dc1cc0d8655925a0e28d50ab4a9f39b6e7979d3b
killbizz <45452791+killbizz@users.noreply.github.com>
2022-06-09 00:50:53 +0200

    improved 500 error page

--- pages/500.tsx
+++ pages/500.tsx
@@ -5,16 +5,18 @@ const CustomError = () => {
   return (
     <>
       <Layout title="Error">
-        <h1 className="text-center my-4">Oooops</h1>
-        <h2 className="text-center my-4 ">
-          A Server-side Error occurred 🤔
+        <h1 className="text-center my-5">Oooops ...</h1>
+        <h2 className="text-center mb-5">
+          An Internal Server Error occurred 🤔
         </h2>
-        <h2 className="text-center my-5">Please remember that this is a PROTOTYPE website and:</h2>
-        <ul className="text-center my-2">
-            <li>The website security was not the focus of the project</li>
-            <li>There may be server-side errors or performance problems due to the database</li>
+        <div className="image-cropper mx-auto text-center">
+          <img src="/images/500_error_2.png" className="d-block rounded" alt="500 Internal Server Error" width="400" height="300" loading="lazy" />
+        </div>
+        <ul className="text-center my-4">
+            <li className="my-2"><h3 className="d-inline">Please remember that this is a PROTOTYPE website</h3></li>
+            <li className="my-2"><h3 className="d-inline">There may be server-side errors or performance problems due to the database</h3></li>
         </ul>
-        <h4 className="text-center my-5">
+        <h4 className="text-center my-4">
           Go back to the
           <Link href="/"> Homepage </Link>
         </h4>
